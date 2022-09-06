5eef25eaaab7f4b5d4327a9ce4d6512b4fbd2f02
Alik Vodyanov <alik.vodyanov@gmail.com>
2022-09-06 16:18:37 +0100

    connectAnalytics + manifest

---
+++ examples/connectAnalytics/app.R
@@ -0,0 +1,7 @@
+library(connectAnalytics)
+library(shiny)
+# Need to set the environment variables on the server
+# Sys.setenv(CONNECT_SERVER = "https://rstudio-connect.heor.co.uk",
+#            CONNECT_API_KEY = "YOUR_API_KEY")
+# Run
+connectAnalytics()
